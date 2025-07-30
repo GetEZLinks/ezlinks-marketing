19dee208b43a55a42dc64a81b804585a96db811d
rup2701 <rupesh@creativegray.com>
2025-07-30 11:54:28 -0700

    Added features section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // src/app/page.tsx
 import Header from '@/components/Header/Header';
 import Hero from '@/components/Hero/Hero';
+import Features from '@/components/Features/Features';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 }
